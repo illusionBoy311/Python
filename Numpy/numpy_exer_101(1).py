8cb4b2bf932f128e4ab474387939765a2381dcd6
illusionBoy311 <hbwhxgychy@163.com>
2021-06-14 16:32:22 +0800

    pandas_exer(101)

---
+++ Numpy/numpy_exer_101(1).py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/14 16:14
+# @Author  : Hu-y
+# @File    : numpy_exer_101(1).py
+#1.Import numpy as np and see the version
+import numpy as np
+print(np.__version__)
+"""
+output:
+1.13.3
+"""
+
+# 2. How to create a 1D array?
+arr = np.arange(10)
+print(arr)
+"""
+output:
+[0 1 2 3 4 5 6 7 8 9]
+"""
